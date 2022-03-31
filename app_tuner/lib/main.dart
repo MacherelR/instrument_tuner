0abb3c36bca739bd9c644618e89fd96d1799a8cc
Denis Rosset <denisrosset2@gmail.com>
2022-03-31 12:46:24 +0200

    added multiples pages and graph on front page

--- app_tuner/lib/main.dart
+++ app_tuner/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_audio_capture/flutter_audio_capture.dart';
 import 'package:pitch_detector_dart/pitch_detector.dart';
 import 'package:pitchupdart/instrument_type.dart';
 import 'package:pitchupdart/pitch_handler.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,22 +18,74 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      initialRoute: '/home',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/home': (context) => const Tuner(title: 'Tuner'),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/stats': (context) => const StatsScreen(),
+        '/settings': (context) => const SettingsScreen(),
+      },
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Tuner(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class Tuner extends StatefulWidget {
+  const Tuner({Key? key, required this.title}) : super(key: key);
   final String title;
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Tuner> createState() => _TunerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class StatsScreen extends StatelessWidget {
+  const StatsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stats'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first screen when tapped.
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Options'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first screen when tapped.
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class _TunerState extends State<Tuner> {
   final _audioRecorder = FlutterAudioCapture();
   final pitchDetectorDart = PitchDetector(44100, 2000);
   // TODO : change instrumentType to the one selected
@@ -77,6 +130,63 @@ class _MyHomePageState extends State<MyHomePage> {
     print(err);
   }
 
+  List<_ChartData>? chartData;
+
+  @override
+  void initState() {
+    chartData = <_ChartData>[
+      _ChartData(2005, 21, 28),
+      _ChartData(2006, 24, 44),
+      _ChartData(2007, 36, 48),
+      _ChartData(2008, 38, 50),
+      _ChartData(2009, 54, 66),
+      _ChartData(2010, 57, 78),
+      _ChartData(2011, 70, 84)
+    ];
+    super.initState();
+  }
+
+  List<LineSeries<_ChartData, num>> _getDefaultLineSeries() {
+    return <LineSeries<_ChartData, num>>[
+      LineSeries<_ChartData, num>(
+          animationDuration: 2500,
+          dataSource: chartData!,
+          xValueMapper: (_ChartData sales, _) => sales.x,
+          yValueMapper: (_ChartData sales, _) => sales.y,
+          width: 2,
+          name: 'Germany',
+          markerSettings: const MarkerSettings(isVisible: true)),
+      LineSeries<_ChartData, num>(
+          animationDuration: 2500,
+          dataSource: chartData!,
+          width: 2,
+          name: 'England',
+          xValueMapper: (_ChartData sales, _) => sales.x,
+          yValueMapper: (_ChartData sales, _) => sales.y2,
+          markerSettings: const MarkerSettings(isVisible: true))
+    ];
+  }
+
+  SfCartesianChart _buildDefaultLineChart(isCardView) {
+    return SfCartesianChart(
+      plotAreaBorderWidth: 0,
+      title: ChartTitle(text: isCardView ? '' : 'Inflation - Consumer price'),
+      legend: Legend(
+          isVisible: isCardView ? false : true,
+          overflowMode: LegendItemOverflowMode.wrap),
+      primaryXAxis: NumericAxis(
+          edgeLabelPlacement: EdgeLabelPlacement.shift,
+          interval: 2,
+          majorGridLines: const MajorGridLines(width: 0)),
+      primaryYAxis: NumericAxis(
+          labelFormat: '{value}%',
+          axisLine: const AxisLine(width: 0),
+          majorTickLines: const MajorTickLines(color: Colors.transparent)),
+      series: _getDefaultLineSeries(),
+      tooltipBehavior: TooltipBehavior(enable: true),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,12 +201,20 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Text("Drawer Header"),
             ),
             ListTile(
-              title: const Text('Item 1'),
+              title: const Text('Stats'),
               onTap: () {
                 // TODO : Define navigation
                 Navigator.pop(context);
+                Navigator.pushNamed(context, '/stats');
               },
-            )
+            ),
+            ListTile(
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/settings');
+              },
+            ),
           ],
         ),
       ),
@@ -123,22 +241,33 @@ class _MyHomePageState extends State<MyHomePage> {
                 fontWeight: FontWeight.bold),
           )),
           Expanded(
-              child: Row(
-            children: [
-              Expanded(
-                  child: Center(
-                      child: FloatingActionButton(
-                          onPressed: _startRecording,
-                          child: const Text("Start")))),
-              Expanded(
-                  child: Center(
-                      child: FloatingActionButton(
-                          onPressed: _stopRecording,
-                          child: const Text("Stop")))),
-            ],
-          ))
+            child: Row(
+              children: [
+                Expanded(
+                    child: Center(
+                        child: FloatingActionButton(
+                            heroTag: "start",
+                            onPressed: _startRecording,
+                            child: const Text("Start")))),
+                Expanded(
+                    child: Center(
+                        child: FloatingActionButton(
+                            heroTag: "stop",
+                            onPressed: _stopRecording,
+                            child: const Text("Stop")))),
+              ],
+            ),
+          ),
+          Expanded(child: Center(child: _buildDefaultLineChart(true))),
         ]),
       ),
     );
   }
+}
+
+class _ChartData {
+  _ChartData(this.x, this.y, this.y2);
+  final double x;
+  final double y;
+  final double y2;
 }
